26a4109ed5584e381e11fc3e2896f90a0d6b5388
phoomss <narongsak5az@gmail.com>
2024-12-07 23:17:23 +0700

    .

--- frontend/src/components/admin/Form/EditUser.jsx
+++ frontend/src/components/admin/Form/EditUser.jsx
@@ -5,8 +5,6 @@ import { useNavigate, useParams } from 'react-router-dom';
 
 const EditUser = () => {
     const { id } = useParams();
-
-    console.log(id)
     const navigate = useNavigate();
 
     const [title, setTitle] = useState("");
