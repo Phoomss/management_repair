913e3188a8a9dc0e333702a46b5a6c8e3af75dc3
phoomss <narongsak5az@gmail.com>
2024-12-11 23:07:49 +0700

    set service

---
+++ frontend/src/service/stepTestService.js
@@ -0,0 +1,39 @@
+import http from './http-common'
+
+const createStepTest = async (stepTestData) => {
+    return await http.post('/api/step-test/', stepTestData, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    })
+}
+
+const listStepTest = async () => {
+    return await http.get('/api/step-test')
+}
+
+const stepTestById = async (id) => {
+    return await http.get(`/api/step-test/${id}`)
+}
+
+const updateStepTest = async (id, stepTestData) => {
+    return await http.put(`/api/step-test/${id}`, stepTestData, {
+        headers: {
+            'Content-Type': 'multipart/form-data', // Ensure the content type is set for file uploads
+        },
+    })
+}
+
+const deleteStepTest = async (id) => {
+    return await http.delete(`/api/step-test/${id}`)
+}
+
+const stepTesstService = {
+    createStepTest,
+    listStepTest,
+    stepTestById,
+    updateStepTest,
+    deleteStepTest
+}
+
+export default stepTesstService
